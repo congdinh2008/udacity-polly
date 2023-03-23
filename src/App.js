1543b3885c00dc9137e53d81c1d4611eea23f707
Cong Dinh <congdinhvannguyen165@outlook.com>
2023-03-23 10:42:22 +0700

    Add Login Page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import CreatePoll from "./pages/CreatePoll";
 import Home from "./pages/Home";
 import LeaderBoard from "./pages/LeaderBoard";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Navbar />
       <main className="container">
         <Routes>
+          <Route path="/login" element={<Login />} />
           <Route path="/" element={<Home />} />
           <Route path="/leader-board" element={<LeaderBoard />} />
           <Route path="/new" element={<CreatePoll />} />
---
+++ src/pages/Login.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { FaPoll } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // call your API to authenticate user
+    // assuming the API returns a user object with an id and name property
+    navigate("/");
+  };
+
+  return (
+    <section className="login-section d-flex align-items-center">
+      <div className="login-container">
+        <div className="login-header">
+          <h1>Udacity Polly</h1>
+          <FaPoll />
+        </div>
+        <div className="login-form">
+          <form onSubmit={handleSubmit}>
+            <div className="form-group d-flex flex-column">
+              <label htmlFor="username" className="form-control-label">
+                Username
+              </label>
+              <input
+                type="text"
+                id="username"
+                value={username}
+                onChange={(event) => setUsername(event.target.value)}
+              />
+            </div>
+            <div className="form-group d-flex flex-column">
+              <label htmlFor="password" className="form-control-label">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+              />
+            </div>
+            <button type="submit">Login</button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Login;
