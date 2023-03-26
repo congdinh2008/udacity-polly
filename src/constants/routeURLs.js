4b08aeaa327d2493406d58572d66cb599ecf7093
Cong Dinh <congdinh2012@hotmail.com>
2023-03-26 07:50:33 +0700

    Update URL for leaderboard

--- src/constants/routeURLs.js
+++ src/constants/routeURLs.js
@@ -1,7 +1,7 @@
 const RouteURLs = {
   HOME_URL: "/",
   LOGIN_URL: "/login",
-  LEADER_BOARD_URL: "/leader-board",
+  LEADER_BOARD_URL: "/leaderboard",
   CREATE_POLL_URL: "/add",
   POLL_ITEM_URL: "/questions/:id",
   NOT_FOUND_URL: "/not-found",
